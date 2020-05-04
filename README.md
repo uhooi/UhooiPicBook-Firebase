bdfded51a294679a25662c87944a64354ac9dee2
uhooi <uhooi@server.fake>
2020-05-04 18:26:44 +0900

    Add Firestore security rules testing

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # UhooiPicBook-Firebase
-Uhooi's character book.
+
+![logo](./docs/logo.png)
+
+Testing Firestore security rules for Uhooi's character book.
+
+## Environment
+
+- Node.js: v12.14.0
+- npm: 6.13.4
+
+## Setup
+
+1. Clone the project.
+
+```
+$ git clone https://github.com/uhooi/UhooiPicBook-Firebase.git
+$ cd UhooiPicBook-Firebase
+```
+
+2. Install Firebase CLI.
+
+```
+$ npm install -g firebase-tools
+```
+
+3. Install the emulator.
+
+```
+$ firebase setup:emulators:firestore
+```
+
+4. Install packages managed by npm.
+
+```
+$ npm install
+```
+
+## Testing
+
+Run `npm test` .
+
+## References
+
+- https://firebase.google.com/docs/cli/
+- https://firebase.google.com/docs/firestore/security/test-rules-emulator
+- https://github.com/firebase/quickstart-nodejs/tree/master/firestore-emulator/typescript-quickstart
+- https://github.com/mogaming217/youtube-simple-todo-sample
---
+++ tests/monsters-tests.ts
@@ -0,0 +1,162 @@
+import * as firebase from '@firebase/testing'
+import * as fs from 'fs'
+
+//#region Types
+
+type Auth = {
+  uid?: string,
+  [key: string]: any
+}
+
+//#endregion
+
+//#region Consts
+
+const projectId = 'uhooipicbook'
+const databaseName = 'uhooipicbook'
+const rules = fs.readFileSync('./firestore.rules', 'utf8')
+const authedApp = (auth?: Auth) => firebase.initializeTestApp({ projectId: projectId, databaseName, auth }).firestore()
+const adminApp =firebase.initializeAdminApp({ projectId: projectId, databaseName }).firestore()
+const coverageUrl = `http://localhost:8080/emulator/v1/projects/${projectId}:ruleCoverage.html`
+
+//#endregion
+
+//#region TestCase Life-Cycle Methods
+
+beforeAll(async () => {
+  await firebase.loadFirestoreRules({ projectId: projectId, rules })
+})
+
+beforeEach(async () => {
+  await firebase.clearFirestoreData({ projectId: projectId })
+})
+
+afterAll(async () => {
+  await Promise.all(firebase.apps().map(app => app.delete()))
+  console.log(`View rule coverage information at ${coverageUrl}\n`)
+})
+
+//#endregion
+
+//#region Test Methods
+
+describe('/monsters', () => {
+  describe('create', () => {
+    it('can not create', async () => {
+      const db = authedApp(null)
+
+      await firebase.assertFails(db.collection('monsters').doc('uhooi').set({
+        name: 'uhooi',
+        description: 'ゆかいな　みどりの　せいぶつ。\nわるそうに　みえるが　むがい。',
+        base_color: '#FFFFFF',
+        icon_url: 'https://example.com/example.png',
+        dancing_url: 'https://example.com/example.gif',
+        order: 1
+      }))
+    })
+  })
+
+  describe('update', () => {
+    it('can not update', async () => {
+      createTestData('monsters', 'uhooi')
+      const db = authedApp(null)
+
+      await firebase.assertFails(db.collection('monsters').doc('uhooi').set({
+        name: 'uhooi',
+        description: 'ゆかいな　みどりの　せいぶつ。\nわるそうに　みえるが　むがい。',
+        base_color: '#FFFFFF',
+        icon_url: 'https://example.com/example.png',
+        dancing_url: 'https://example.com/example.gif',
+        order: 1
+      }))
+    })
+  })
+
+  describe('get', () => {
+    it('can not get', async () => {
+      createTestData('monsters', 'uhooi')
+      const db = authedApp(null)
+
+      await firebase.assertFails(db.collection('monsters').doc('uhooi').get())
+    })
+  })
+  
+  describe('list', () => {
+    it('can get list', async () => {
+      createTestData('monsters', 'uhooi')
+      const db = authedApp(null)
+
+      await firebase.assertSucceeds(db.collection('monsters').get())
+    })
+  })
+})
+
+describe('/others', () => {
+  describe('create', () => {
+    it('can not create', async () => {
+      const db = authedApp(null)
+
+      await firebase.assertFails(db.collection('others').doc('uhooi').set({
+        name: 'uhooi',
+        description: 'ゆかいな　みどりの　せいぶつ。\nわるそうに　みえるが　むがい。',
+        base_color: '#FFFFFF',
+        icon_url: 'https://example.com/example.png',
+        dancing_url: 'https://example.com/example.gif',
+        order: 1
+      }))
+    })
+  })
+
+  describe('update', () => {
+    it('can not update', async () => {
+      createTestData('others', 'uhooi')
+      const db = authedApp(null)
+
+      await firebase.assertFails(db.collection('others').doc('uhooi').set({
+        name: 'uhooi',
+        description: 'ゆかいな　みどりの　せいぶつ。\nわるそうに　みえるが　むがい。',
+        base_color: '#FFFFFF',
+        icon_url: 'https://example.com/example.png',
+        dancing_url: 'https://example.com/example.gif',
+        order: 1
+      }))
+    })
+  })
+
+  describe('get', () => {
+    it('can not get', async () => {
+      createTestData('others', 'uhooi')
+      const db = authedApp(null)
+
+      await firebase.assertFails(db.collection('others').doc('uhooi').get())
+    })
+  })
+  
+  describe('list', () => {
+    it('can not get list', async () => {
+      createTestData('others', 'uhooi')
+      const db = authedApp(null)
+
+      await firebase.assertFails(db.collection('others').get())
+    })
+  })
+})
+
+//#endregion
+
+//#region Other Methods
+
+function createTestData(collectionId: string, documentId: string) {
+  const db = adminApp
+
+  db.collection(collectionId).doc(documentId).set({
+    name: 'uhooi',
+    description: 'ゆかいな　みどりの　せいぶつ。\nわるそうに　みえるが　むがい。',
+    base_color: '#FFFFFF',
+    icon_url: 'https://example.com/example.png',
+    dancing_url: 'https://example.com/example.gif',
+    order: 1
+  })
+}
+
+//#endregion
